4388e7c97a7410e83e2ad419b824b6180e2e759a
BaiToshoo <toshetos04@gmail.com>
2024-06-30 14:41:27 +0300

    04.print-and-sum done

---
+++ 01. JavaScript Syntax Fundamentals/04.print-and-sum.js
@@ -0,0 +1,13 @@
+function sumAndPrint(firstNumber, secondNumber) {
+    
+    let output = "";
+    let sum = 0;
+    for (let currentNumber = firstNumber; currentNumber <= secondNumber; currentNumber++) {
+        output += currentNumber + " ";
+        sum += currentNumber;
+    }
+    console.log(output.trim());
+    console.log(`Sum: ${sum}`);
+}
+
+sumAndPrint(50, 60);
