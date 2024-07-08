a4555690e75a7c965126480fda1e16170be7fd37
BaiToshoo <toshetos04@gmail.com>
2024-07-09 00:23:15 +0300

    02.print-every-n-element done

---
+++ 02. JavaScript Arrays and Strings/02.print-every-n-element.js
@@ -0,0 +1,10 @@
+function action(array, n) {
+    let returnArray = [];
+    for (let i = 0; i < array.length; i += n) {
+        returnArray.push(array[i]);
+        array.filter
+    }
+    return returnArray;
+}
+
+action(['1','2','3','4','5'],6); // 1
