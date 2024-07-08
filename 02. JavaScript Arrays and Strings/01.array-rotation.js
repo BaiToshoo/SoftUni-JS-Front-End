05e74f7bbcdf38a1ed548c9f4147a9919b2e88e4
BaiToshoo <toshetos04@gmail.com>
2024-07-08 19:53:33 +0300

    Different solution for 01.array-rotation

--- 02. JavaScript Arrays and Strings/01.array-rotation.js
+++ 02. JavaScript Arrays and Strings/01.array-rotation.js
@@ -1,14 +1,19 @@
 function ArrayRotation(array, nOfRotations) {
 
-    for (let i = 0; i < nOfRotations; i++) {
-        let temp = array[0];
-        for (let j = 0; j < array.length - 1; j++) {
-            array[j] = array[j + 1];
-        }
-        array[array.length - 1] = temp;
-    }
+    // for (let i = 0; i < nOfRotations; i++) {
+    //     let temp = array[0];
+    //     for (let j = 0; j < array.length - 1; j++) {
+    //         array[j] = array[j + 1];
+    //     }
+    //     array[array.length - 1] = temp;
+    // }
 
-    return array.join(' ');
+    // return array.join(' ');
+
+    const cutOffIndex = nOfRotations % array.length;
+    const firstPart = array.slice(0, cutOffIndex);
+    const secondPart = array.slice(cutOffIndex);
+    return secondPart.concat(firstPart);
 }
 
 console.log(ArrayRotation([51, 47, 32, 61, 21], 2)); // [32, 61, 21, 51, 47]
