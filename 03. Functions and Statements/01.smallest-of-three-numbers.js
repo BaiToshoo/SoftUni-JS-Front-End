ac7d7753f8d35f6d0003075af4bb1b1ae5f01f13
BaiToshoo <toshetos04@gmail.com>
2024-07-19 12:21:09 +0300

    01.smallest-of-three-numbers done

---
+++ 03. Functions and Statements/01.smallest-of-three-numbers.js
@@ -0,0 +1,6 @@
+function FindSmallestNumberOfThreeNumbers(a, b, c) {
+    let smallestNumber = Math.min(a, b, c);
+    return smallestNumber;
+}
+
+console.log(FindSmallestNumberOfThreeNumbers(2, 5, 3)); // 2
