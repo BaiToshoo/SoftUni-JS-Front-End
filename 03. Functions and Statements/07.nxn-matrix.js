bd5466b45286a2a5a90dfcc2122f829b66fa6310
BaiToshoo <toshetos04@gmail.com>
2024-07-19 14:39:12 +0300

    07.nxn-matrix done

---
+++ 03. Functions and Statements/07.nxn-matrix.js
@@ -0,0 +1,9 @@
+function nxnMatrix(n){
+    for (let i = 0; i < n; i++) {
+        console.log(`${n} `.repeat(n));
+    }
+}
+
+nxnMatrix(3); // 3 3 3
+              // 3 3 3
+              // 3 3 3
