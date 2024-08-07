97237a2317a40fe70bd92af52685ce714bfcf941
BaiToshoo <toshetos04@gmail.com>
2024-08-07 13:24:35 +0300

    01.employees done

---
+++ 04. Objects and Classes/01.employees.js
@@ -0,0 +1,16 @@
+function Employees(input) {
+    let employees = [];
+
+    for (let i = 0; i < input.length; i++) {
+        employees.push(employee = {
+            name: input[i],
+            personalNumber: input[i].length
+        });
+    }
+
+    for (const employee of employees) {
+        console.log(`Name: ${employee.name} -- Personal Number: ${employee.personalNumber}`);
+    }
+}
+
+Employees(['Silas Butler', 'Adnaan Buckley', 'Juan Peterson', 'Brendan Villarreal']); // Name: Silas Butler -- Personal Number: 12
