245a83e298cb14c72c8a28ecd27b124e63b49ad7
BaiToshoo <toshetos04@gmail.com>
2024-08-07 13:35:55 +0300

    02.towns done

---
+++ 04. Objects and Classes/02.towns.js
@@ -0,0 +1,19 @@
+function Towns(input){
+    input = input.map(x => x.split(' | '));
+
+    let towns = [];
+
+    for (let i = 0; i < input.length; i++) {
+        towns.push(town = {
+            town: input[i][0],
+            latitude: Number(input[i][1]).toFixed(2),
+            longitude: Number(input[i][2]).toFixed(2)
+        });
+    }
+
+    for (const town of towns) {
+        console.log(town);
+    }
+}
+
+Towns(['Sofia | 42.696552 | 23.32601', 'Beijing | 39.913818 | 116.363625']); // Name: Sofia -- Latitude: 42.70 -- Longitude: 23.33
