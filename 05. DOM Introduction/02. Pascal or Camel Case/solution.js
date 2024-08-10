3a958f9353375742aa7b335b79e2442c86761868
BaiToshoo <toshetos04@gmail.com>
2024-08-10 13:19:50 +0300

    02. Pascal or Camel Case done

--- 05. DOM Introduction/02. Pascal or Camel Case/solution.js
+++ 05. DOM Introduction/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,21 @@
 function solve() {
-  //TODO...
+  const input = document.getElementById('text').value.toLowerCase().split(' ');
+  const currentCase = document.getElementById('naming-convention').value;
+
+  let result = '';
+
+  if (currentCase === 'Camel Case') {
+    result += input[0];
+    for (let i = 1; i < input.length; i++) {
+      result += input[i][0].toUpperCase() + input[i].slice(1);
+    }
+  } else if (currentCase === 'Pascal Case') {
+    for (let word of input) {
+      result += word[0].toUpperCase() + word.slice(1);
+    }
+  } else {
+    result = 'Error!';
+  }
+
+  document.getElementById('result').textContent = result;
 }
