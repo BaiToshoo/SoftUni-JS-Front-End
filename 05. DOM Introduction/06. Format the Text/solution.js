2e3b90563969a4b9270e4eb9fbfdb37d44125d6e
BaiToshoo <toshetos04@gmail.com>
2024-08-10 14:42:45 +0300

    06. Format the Text done

--- 05. DOM Introduction/06. Format the Text/solution.js
+++ 05. DOM Introduction/06. Format the Text/solution.js
@@ -1,3 +1,12 @@
 function solve() {
-  //TODO
+  const input = document.getElementById('input').value;
+  const output = document.getElementById('output');
+  const sentences = input.split('.').filter(x => x.length > 0);
+
+  for (let i = 0; i < sentences.length; i += 3) {
+    const paragraph = sentences.slice(i, i + 3).join('.') + '.';
+    const p = document.createElement('p');
+    p.textContent = paragraph;
+    output.appendChild(p);
+  }
 }
