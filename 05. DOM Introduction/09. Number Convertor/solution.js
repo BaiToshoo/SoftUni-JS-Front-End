e854cbc6d21246dca723cc556320724a03bf47b2
BaiToshoo <toshetos04@gmail.com>
2024-08-10 16:18:13 +0300

    09. Number Convertor done

--- 05. DOM Introduction/09. Number Convertor/solution.js
+++ 05. DOM Introduction/09. Number Convertor/solution.js
@@ -1,4 +1,30 @@
 function solve() {
+    let input = document.getElementById('input');
+    let selectToMenu = document.getElementById('selectMenuTo');
+    let result = document.getElementById('result');
 
-    //TODO...
+    let binaryOption = document.createElement('option');
+    binaryOption.textContent = 'Binary';
+    binaryOption.value = 'binary';
+
+    let hexadecimalOption = document.createElement('option');
+    hexadecimalOption.textContent = 'Hexadecimal';
+    hexadecimalOption.value = 'hexadecimal';
+
+    selectToMenu.appendChild(binaryOption);
+    selectToMenu.appendChild(hexadecimalOption);
+
+    let convertButton = document.getElementsByTagName('button')[0];
+    convertButton.addEventListener('click', () => {
+        let number = Number(input.value);
+        let resultValue = '';
+
+        if (selectToMenu.value === 'binary') {
+            resultValue = number.toString(2);
+        } else if (selectToMenu.value === 'hexadecimal') {
+            resultValue = number.toString(16).toUpperCase();
+        }
+
+        result.value = resultValue;
+    });
 }
