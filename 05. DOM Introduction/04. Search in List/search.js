e90a27266751323e97ed666a16f6a3c9dd0c7cd5
BaiToshoo <toshetos04@gmail.com>
2024-08-10 13:59:31 +0300

    04. Search in List done

--- 05. DOM Introduction/04. Search in List/search.js
+++ 05. DOM Introduction/04. Search in List/search.js
@@ -1,3 +1,19 @@
 function search() {
-   // TODO
+   const towns = Array.from(document.querySelectorAll('ul li'));
+   const searchField = document.getElementById('searchText');
+   const result = document.getElementById('result');
+
+   let matches = 0;
+   towns.forEach(town => {
+      if (town.textContent.includes(searchField.value)) {
+         town.style.fontWeight = 'bold';
+         town.style.textDecoration = 'underline';
+         matches++;
+      } else {
+         town.style.fontWeight = '';
+         town.style.textDecoration = '';
+      }
+   });
+
+   result.textContent = `${matches} matches found`;
 }
