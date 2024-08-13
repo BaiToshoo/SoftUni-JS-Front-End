f6d19cc84b0e490eecc635e552df74afcf27bcd0
BaiToshoo <toshetos04@gmail.com>
2024-08-13 16:49:57 +0300

    03. Locked-Profile done

--- 10. DOM Manipulation and Events/03. Locked-Profile/app.js
+++ 10. DOM Manipulation and Events/03. Locked-Profile/app.js
@@ -1,3 +1,25 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const buttons = Array.from(document.querySelectorAll('button'));
+    buttons.forEach(b => b.addEventListener('click', showHide));
+
+    function showHide(e) {
+        const button = e.target;
+        const profile = button.parentElement;
+        const isLocked = profile.querySelector('input[type="radio"][value="lock"]').checked;
+
+        if (isLocked) {
+            return;
+        }
+
+        const hiddenInformation = profile.querySelector('div');
+        const isVisible = hiddenInformation.style.display === 'block';
+
+        if (isVisible) {
+            hiddenInformation.style.display = 'none';
+            button.textContent = 'Show more';
+        } else {
+            hiddenInformation.style.display = 'block';
+            button.textContent = 'Hide it';
+        }
+    }
 }
