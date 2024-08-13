eef87b72318a816e44cb43b298dea6fcd0c7fcaa
BaiToshoo <toshetos04@gmail.com>
2024-08-13 16:27:17 +0300

    01. Sections done

--- 10. DOM Manipulation and Events/01. Sections/script.js
+++ 10. DOM Manipulation and Events/01. Sections/script.js
@@ -1,3 +1,14 @@
 function create(words) {
-   console.log('TODO:...');
+   words.forEach(word => {
+      let div = document.createElement('div');
+      let p = document.createElement('p');
+      p.textContent = word;
+      p.style.display = 'none';
+      div.appendChild(p);
+      div.addEventListener('click', () => {
+         p.style.display = 'block';
+      });
+      document.getElementById('content').appendChild(div);
+
+   });
 }
