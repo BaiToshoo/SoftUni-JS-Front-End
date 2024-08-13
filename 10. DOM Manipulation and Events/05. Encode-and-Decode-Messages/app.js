c50848d52f059c2f7d7180e41d0b66afad012326
BaiToshoo <toshetos04@gmail.com>
2024-08-13 19:23:36 +0300

    05. Encode-and-Decode-Messages done

--- 10. DOM Manipulation and Events/05. Encode-and-Decode-Messages/app.js
+++ 10. DOM Manipulation and Events/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,27 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const textAreas = document.querySelectorAll('textarea');
+    const buttons = document.querySelectorAll('button');
+    const [encodeButton, decodeButton] = buttons;
+    const [encodeTextArea, decodeTextArea] = textAreas;
+
+    encodeButton.addEventListener('click', () => {
+        let message = encodeTextArea.value
+            .split('')
+            .map((char) => char.charCodeAt() + 1)
+            .map((charCode) => String.fromCharCode(charCode))
+            .join('');
+
+        decodeTextArea.value = message;
+        encodeTextArea.value = '';
+    });
+
+    decodeButton.addEventListener('click', () => {
+        let message = decodeTextArea.value
+            .split('')
+            .map((char) => char.charCodeAt() - 1)
+            .map((charCode) => String.fromCharCode(charCode))
+            .join('');
+
+        decodeTextArea.value = message;
+    });
 }
