e0ab86e10253fe4fb22120d612b5827caa090b7b
BaiToshoo <toshetos04@gmail.com>
2024-08-17 17:11:25 +0300

    03.The Step Challenge-Add a Record done and made some architectural changes

--- 07. Exam Preparation/03.The Step Challenge/app.js
+++ 07. Exam Preparation/03.The Step Challenge/app.js
@@ -2,18 +2,15 @@ const baseURL = 'http://localhost:3030/jsonstore/records';
 
 const loadButton = document.getElementById('load-records');
 const recordList = document.getElementById('list');
+const addButton = document.getElementById('add-record');
 
-loadButton.addEventListener('click', async () => {
-    recordList.innerHTML = '';
-    const response = await fetch(baseURL);
-    const result = await response.json();
-    const records = Object.values(result);
+const nameInput = document.getElementById('p-name');
+const stepsInput = document.getElementById('steps');
+const caloriesInput = document.getElementById('calories');
 
-    const recordElements = records.map( record => createRecordElement(record.name, record.steps, record.calories, record._id))
+loadButton.addEventListener('click', loadRecords);
 
-    recordList.append(...recordElements);
-
-});
+addButton.addEventListener('click', addRecords);
 
 function createRecordElement(name, steps, calories, _id) {
     const pName = document.createElement('p');
@@ -50,4 +47,38 @@ function createRecordElement(name, steps, calories, _id) {
     recordLi.appendChild(buttonDiv);
 
     return recordLi;
+}
+
+async function loadRecords(){
+    recordList.innerHTML = '';
+    const response = await fetch(baseURL);
+    const result = await response.json();
+    const records = Object.values(result);
+
+    const recordElements = records.map( record => createRecordElement(record.name, record.steps, record.calories, record._id))
+
+    recordList.append(...recordElements);
+}
+
+async function addRecords() {
+    const name = nameInput.value;
+    const steps = stepsInput.value;
+    const calories = caloriesInput.value;
+
+    clearInputFields();
+
+    await fetch(baseURL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ name, steps, calories })
+    });
+
+    await loadRecords();
+
+}
+
+function clearInputFields() {
+    nameInput.value = '';
+    stepsInput.value = '';
+    caloriesInput.value = '';
 }
